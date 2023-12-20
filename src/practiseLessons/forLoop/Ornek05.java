38d6047fc2565ced9c2be63934973d50c4429f71
ChatGTHB <cihat.kose@hotmail.com>
2023-12-20 13:36:02 +0100

    Added a for loop example

--- src/practiseLessons/forLoop/Ornek05.java
+++ src/practiseLessons/forLoop/Ornek05.java
@@ -3,40 +3,22 @@ package practiseLessons.forLoop;
 import java.util.Scanner;
 
 public class Ornek05 {
-
     public static void main(String[] args) {
 
-        /**
-         Kullanıcıdan kaç adet sayı gireceğini isteyen ve
-         bu sayıların en büyüğünü ile en küçüğünü bulan
-         Java programını yazınız.
-         */
+        // Kullanıcıdan alınan sayının faktöriyelini veren Java programı yazınız.
+
+        // Faktöriyel, pozitif bir tam sayının kendisi ile 1 arasındaki tüm tam sayılarla çarpımıdır.
+        // Örneğin, 5 faktöriyel (5!) 5 x 4 x 3 x 2 x 1 = 120 olarak hesaplanır.
 
         Scanner input = new Scanner(System.in);
+        System.out.print("Bir sayı girin: ");
+        int sayi = input.nextInt();
 
-        System.out.print("Kaç tane sayı gireceğinizi belirtin: ");
-        int n = input.nextInt();
-
-        int enBuyuk = Integer.MIN_VALUE;
-        int enKucuk = Integer.MAX_VALUE;
-
-        for (int i = 0; i < n; i++) {
-            System.out.print("Bir sayı girin: ");
-            int num = input.nextInt();
-
-            if (num > enBuyuk) {
-                enBuyuk = num;
-            }
-
-            if (num < enKucuk) {
-                enKucuk = num;
-            }
+        int faktoriyel = 1;
+        for (int i = 1; i <= sayi; i++) {
+            faktoriyel *= i;
         }
 
-        System.out.println("En büyük sayı: " + enBuyuk);
-        System.out.println("En küçük sayı: " + enKucuk);
-
-        input.close();
-
+        System.out.println(sayi + " sayısının faktöriyeli: " + faktoriyel);
     }
 }
