054d2c40667ca3ee3484ce48b94b35f1f2adc772
ChatGTHB <cihat.kose@hotmail.com>
2023-12-21 14:18:58 +0100

    Updated Ornek06.java

--- src/practiseLessons/forLoop/Ornek06.java
+++ src/practiseLessons/forLoop/Ornek06.java
@@ -3,40 +3,25 @@ package practiseLessons.forLoop;
 import java.util.Scanner;
 
 public class Ornek06 {
-
     public static void main(String[] args) {
 
-        /**
-         Kullanıcıdan kaç adet sayı gireceğini isteyen ve
-         bu sayıların en büyüğünü ile en küçüğünü bulan
-         Java programını yazınız.
-         */
+        // Kullanıcıdan bir kelime alıp, aşağıdaki gibi yazdıran Java programı yazınız.
+        // Girilen metin: "Kelime"
+
+        // e
+        // im
+        // ime
+        // lime
+        // elime
+        // Kelime
 
         Scanner input = new Scanner(System.in);
 
-        System.out.print("Kaç tane sayı gireceğinizi belirtin: ");
-        int n = input.nextInt();
+        System.out.print("Lütfen bir kelime girin: ");
+        String kelime = input.nextLine();
 
-        int enBuyuk = Integer.MIN_VALUE;
-        int enKucuk = Integer.MAX_VALUE;
-
-        for (int i = 0; i < n; i++) {
-            System.out.print("Bir sayı girin: ");
-            int num = input.nextInt();
-
-            if (num > enBuyuk) {
-                enBuyuk = num;
-            }
-
-            if (num < enKucuk) {
-                enKucuk = num;
-            }
+        for (int i = kelime.length() - 1; i >= 0; i--) {
+            System.out.println("// "+ kelime.substring(i));
         }
-
-        System.out.println("En büyük sayı: " + enBuyuk);
-        System.out.println("En küçük sayı: " + enKucuk);
-
-        input.close();
-
     }
 }
