5aefccab14202f02e5be9c828117c14a0384aaa1
ChatGTHB <cihat.kose@hotmail.com>
2023-12-25 16:35:38 +0100

    Added an 2D_Arrays example

---
+++ src/practiseLessons/array2D/Ornek05.java
@@ -0,0 +1,20 @@
+package practiseLessons.array2D;
+
+public class Ornek05 {
+
+    public static void main(String[] args) {
+
+        int[][] matrix = {{1, 2, 3}, {4, 5, 6}, {7, 8, 9}};
+        // matrix'in her bir sütunundaki elemanların toplamını ekrana yazdırın.
+
+        for (int i = 0; i < matrix[0].length; i++) {
+            int sutunToplam = 0;
+            for (int j = 0; j < matrix.length; j++) {
+                if (i < matrix[j].length) {
+                    sutunToplam += matrix[j][i];
+                }
+            }
+            System.out.println("Sütun " + i + " toplamı: " + sutunToplam);
+        }
+    }
+}
