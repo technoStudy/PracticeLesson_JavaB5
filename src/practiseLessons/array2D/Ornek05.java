fe2c6d2a62bcff5eac7738f2f02748a9e4195580
ChatGTHB <cihat.kose@hotmail.com>
2023-12-27 17:37:21 +0100

    Edited

--- src/practiseLessons/array2D/Ornek05.java
+++ src/practiseLessons/array2D/Ornek05.java
@@ -7,14 +7,12 @@ public class Ornek05 {
         int[][] matrix = {{1, 2, 3}, {4, 5, 6}, {7, 8, 9}};
         // matrix'in her bir sütunundaki elemanların toplamını ekrana yazdırın.
 
-        for (int i = 0; i < matrix[0].length; i++) {
+        for (int i = 0; i < matrix.length; i++) {
             int sutunToplam = 0;
-            for (int j = 0; j < matrix.length; j++) {
-                if (i < matrix[j].length) {
+            for (int j = 0; j < matrix[i].length; j++) {
                     sutunToplam += matrix[j][i];
-                }
             }
-            System.out.println("Sütun " + i + " toplamı: " + sutunToplam);
+            System.out.println("Sütun " + (i+1) + " toplamı: " + sutunToplam);
         }
     }
 }
