0a447c793ae841aefe3bed0d68159863e6f92ad4
ChatGTHB <cihat.kose@hotmail.com>
2024-01-23 10:07:08 +0100

    Added an example about inheritance

---
+++ src/practiseLessons/inheritance/ornek01/Hayvan.java
@@ -0,0 +1,13 @@
+package practiseLessons.inheritance.ornek01;
+
+// Hayvan sınıfı
+public class Hayvan {
+    String renk;
+    int yas;
+
+    // Hayvan sınıfının yapıcı metodu
+    public Hayvan(String renk, int yas) {
+        this.renk = renk;
+        this.yas = yas;
+    }
+}
