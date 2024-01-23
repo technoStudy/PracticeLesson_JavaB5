72fb86b38af3fda653c4b5fd4edb23fb4ea27900
ChatGTHB <cihat.kose@hotmail.com>
2024-01-23 08:19:02 +0100

    Edited

--- src/practiseLessons/encapsulation/ornek01/Ornek01.java
+++ src/practiseLessons/encapsulation/ornek01/Ornek01.java
@@ -6,7 +6,7 @@ public class Ornek01 {
 
         Kisi kisi = new Kisi();
         kisi.ad = "Hazar"; // Kişi nesnesi oluşturulur ve "ad" özelliği "Hazar" olarak atanır
-        kisi.setYas(30); // "setYas" metodu kullanılarak "yas" özelliği 30 olarak ayarlanır
+        kisi.setYas(30);   // "setYas" metodu kullanılarak "yas" özelliği 30 olarak ayarlanır
 
         int yas = kisi.getYas(); // "getYas" metodu kullanılarak kişinin yaş değeri alınır
         System.out.println("Kişinin yaşı: " + yas); // Kişinin yaş değeri ekrana yazdırılır
