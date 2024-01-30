9dbac594a7ebe73b95d12dae54b1f7cbd2313542
ChatGTHB <cihat.kose@hotmail.com>
2024-01-30 23:10:07 +0100

    Added an Abstract Class example

---
+++ src/practiseLessons/abstractClass/ornek01/GeometrikSekil.java
@@ -0,0 +1,8 @@
+package practiseLessons.abstractClass.ornek01;
+
+// GeometrikSekil abstract classı oluşturuldu
+public abstract class GeometrikSekil {
+
+    // Alan hesaplama işlemini gerçekleştiren abstract bir metod tanımlandı
+    public abstract double alanHesapla();
+}
---
+++ src/practiseLessons/abstractClass/ornek01/Ornek01.java
@@ -0,0 +1,18 @@
+package practiseLessons.abstractClass.ornek01;
+
+// main methodun bulunduğu class oluşturuldu
+public class Ornek01 {
+
+    public static void main(String[] args) {
+        // Dikdortgen ve Daire sınıflarından nesneler oluşturulup alanları hesaplandı.
+        Dikdortgen dikdortgen = new Dikdortgen(5, 3);
+        double dikdortgenAlan = dikdortgen.alanHesapla();
+
+        Daire daire = new Daire(4);
+        double daireAlan = daire.alanHesapla();
+
+        // Sonuçlar ekrana yazdırılır.
+        System.out.println("Dikdörtgen Alanı: " + dikdortgenAlan);
+        System.out.println("Daire Alanı: " + daireAlan);
+    }
+}
