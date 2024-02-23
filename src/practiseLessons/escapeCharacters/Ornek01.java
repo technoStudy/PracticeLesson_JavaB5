35b139d8221d37a5272c1bc8456bb486b359f837
ChatGTHB <cihat.kose@hotmail.com>
2024-02-23 18:08:08 +0100

    Edited

--- src/practiseLessons/escapeCharacters/Ornek01.java
+++ src/practiseLessons/escapeCharacters/Ornek01.java
@@ -4,16 +4,19 @@ public class Ornek01 {
 
     public static void main(String[] args) {
 
-/*        Soru 1 :
-          Java'da kaçış karakteri olan \n ne anlama gelir ve nasıl kullanılır? */
+          /*
+           Soru 1 :
+           Java'da kaçış karakteri olan \n ne anlama gelir ve nasıl kullanılır?
+          */
 
         System.out.println("Merhaba,\nDünya!");
         System.out.println("Merhaba, Dünya!");
         System.out.println("G\nU\nN");
 
 
-        /* Soru 2 :
-           Java'da \t kaçış karakteri ne işe yarar?
+        /*
+        Soru 2 :
+        Java'da \t kaçış karakteri ne işe yarar?
         */
         System.out.println("Ne\tHaber");
         System.out.println("Ne Haber");
@@ -26,10 +29,7 @@ public class Ornek01 {
 //        Soru 4 :
 //        Java'da \ (ters bölü işareti) kaçış karakterini kullanarak bir yol (path) ifadesi oluşturun.
 
-                          //C:\Users\muhammed.mavus1\IdeaProjects
+        //C:\Users\muhammed.mavus1\IdeaProjects
         System.out.println("C:\\Users\\muhammed.mavus1\\IdeaProjects");
-
-
-
     }
 }
